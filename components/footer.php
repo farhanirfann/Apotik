6cd0efd7dd1a22d2707007e27611b2dcb5704ebb
ojanadim <161143963+ojanadim@users.noreply.github.com>
2024-04-29 17:25:30 +0700

    header n footer

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,64 @@
-<footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Footer</title>
+<style>
+footer {
+    background-color: #0B9C4E;
+    padding: 20px 0;
+    text-align: center;
+}
 
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.logo img {
+    width: 200px; /* Sesuaikan ukuran logo */
+    height: auto; /* Sesuaikan tinggi logo dengan kebutuhan */
+}
+
+.contact-info {
+    text-align: left;
+}
+
+.contact-info p {
+    margin: 10px 0;
+}
+
+.contact-info ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+.contact-info ul li {
+    margin: 10px 0;
+}
+</style>
+</head>
+<body>
+
+<footer>
+    <div class="container">
+        <div class="logo">
+            <img src="LogoP.png" alt="Logo">
+        </div>
+        <div class="contact-info">
+            <p>Alamat: Menara 165 Jl.Tb.Simatupang, <br>Cilandak Barat, Pasar Minggu, Jakarta Selatan<br></p>
+            <ul>
+                <li>Email: Medicalzeus@gmail.com</li>
+                <li>Telepon: +62891998768</li>
+                <li>WhatsApp: +62889123066</li>
+            </ul>
+        </div>
+    </div>
 </footer>
+
+</body>
+</html>
